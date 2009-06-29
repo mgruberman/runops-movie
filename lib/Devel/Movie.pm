b7154a2041a42be9de159976e6357bd6cdb63e0e
Joshua ben Jore <jbenjore@whitepages.com>
2009-06-29 00:45:47 -0700

    Add Devel::Movie, perl-movie uses it

---
+++ lib/Devel/Movie.pm
@@ -0,0 +1,21 @@
+package Devel::Movie;
+package DB;
+use strict;
+use feature ':5.10';
+use Internals::DumpArenas ();
+sub tracer {
+    state $Frame = 0;
+    local $, = ' ';
+    print STDERR 'Runops::Movie frame ', ++$Frame, @_
+        or warn "Can't write to STDERR: $!";
+    Internals::DumpArenas::DumpArenas();
+}
+
+sub DB { tracer }
+sub sub {
+    no strict;
+    tracer $sub;
+    goto &$sub;
+}
+
+q(Go drinking with mst);
